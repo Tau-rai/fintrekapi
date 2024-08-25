7b7b3712ce12f69832da12cfc24657648d104b40
Taurai <68677265+Tau-rai@users.noreply.github.com>
2024-08-25 23:12:55 +0200

    Update settings.py

--- fintrack/PennyPillar/settings.py
+++ fintrack/PennyPillar/settings.py
@@ -146,8 +146,8 @@ WSGI_APPLICATION = 'PennyPillar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'PennyPillar'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'pennypillardb'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
