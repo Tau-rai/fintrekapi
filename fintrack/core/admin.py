1c862f01d5e39c54bc711fe55da8676a2eb96ead
Taurai <taumasaire@gmail.com>
2024-09-22 09:49:26 +0200

    updating settings.py for production

--- fintrack/core/admin.py
+++ fintrack/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Transaction, Category, MonthlyBudget, UserProfile, SavingsGoal, Subscription, Insight
+
+admin.site.register(Transaction)
+admin.site.register(Category)
+admin.site.register(MonthlyBudget)
+admin.site.register(UserProfile)
+admin.site.register(SavingsGoal)
+admin.site.register(Subscription)
+admin.site.register(Insight)
